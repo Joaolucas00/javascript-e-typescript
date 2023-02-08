93993e5d6df9ce7bfcc8c593cdf2c843e09f4519
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-02-07 22:59:31 -0300

    atualizando

---
+++ ex/ex006/javascript.js
@@ -0,0 +1,12 @@
+console.log('Olá, Mundo!')
+var vel = 112
+
+if (vel > 80) {
+    console.log('Você utrapassou o limite de velocidade! Será multado.')
+} else {
+    console.log(`A velocidade do seu carro é ${vel}. Dirija com cuidado!`)
+}
+
+
+
+
