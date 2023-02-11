ca16ebac883c2c1337b64531a066780119c61dd0
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-02-11 19:45:27 -0300

    fazendo uns testes com vetores em JS

---
+++ ex/ex012/script.js
@@ -0,0 +1,19 @@
+let numeros = [2, 7, 8, 3, 4]
+console.log(numeros.length)
+numeros.push('oLÁ')
+console.log(numeros)
+console.log(numeros.length)
+
+numeros[5] = null
+numeros.sort()
+console.log(numeros)
+
+/*
+let pessoa = [['Juan', 19], ['João', 18], ['Pedro', 20]]
+for (cc = 0; cc <= 2; cc++) {
+    console.log(`Pessoa ${cc}: `)
+    for (c = 0; c <= 1; c++) {
+        console.log(`${pessoa[cc][c]}`)
+    }
+}
+*/
