0df03488223892709f1a73b96ea129dd8e1e4e77
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-02-10 22:44:02 -0300

    Update script.js

--- ex/ex011/script.js
+++ ex/ex011/script.js
@@ -1,9 +1,9 @@
 function gerarTabuada() {
     var txt_tabuada = document.getElementById('inum')
-    var tabuada = Number(txt_tabuada.value)
     var select_tabuada = document.getElementById('itab')
 
     for (var cc = 1; cc <= 10; cc++) {
+        var tabuada = Number(txt_tabuada.value)
         var option = document.createElement('option')
         option.text = `${tabuada} x ${cc} = ${cc * tabuada}`
         select_tabuada.appendChild(option)
