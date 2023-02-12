f3a4f422b3bb632527e08f546843292d193bc206
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-02-11 23:40:09 -0300

    Usando função recursiva

--- ex/ex013/script.js
+++ ex/ex013/script.js
@@ -3,6 +3,14 @@ function calc_porcen(n1=0, porcen=100) {
     return n1*(porcen/100)
 }
 
+function funcao_recursiva_fatorial(n) {
+    if (n==1) {
+        return 1
+    } else {
+        return n * funcao_recursiva_fatorial(n - 1)
+    }
+    
+}
 
 // Function Expression
 const calcPorcen = function(n=0, p=100){return n*(p/100)}
@@ -14,3 +22,6 @@ const calcularPorcentagem = (num=0, por_num=100) => {return num * (por_num/100)}
 console.log(calcularPorcentagem(10, 10)) //Usando Arrow Function
 console.log(calcPorcen(100, 50)) // Usando Function Expression
 console.log(calc_porcen(100, 20)) // Usando uma Function normalmente
+
+
+console.log(funcao_recursiva_fatorial(5))
