b030741b4d987f8a0c449d10f10004f721900aad
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-02-11 23:36:54 -0300

    Testando funções

---
+++ ex/ex013/script.js
@@ -0,0 +1,16 @@
+// Function normal
+function calc_porcen(n1=0, porcen=100) {
+    return n1*(porcen/100)
+}
+
+
+// Function Expression
+const calcPorcen = function(n=0, p=100){return n*(p/100)}
+
+// Arrow Function, a ideai é ser mais simples do que uma função comum.
+const calcularPorcentagem = (num=0, por_num=100) => {return num * (por_num/100)}
+
+
+console.log(calcularPorcentagem(10, 10)) //Usando Arrow Function
+console.log(calcPorcen(100, 50)) // Usando Function Expression
+console.log(calc_porcen(100, 20)) // Usando uma Function normalmente
