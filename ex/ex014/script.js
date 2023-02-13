27dadf9e2c8a6e38af364b4629ceed82f13d52e1
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-02-12 22:06:08 -0300

    terminando o código JS

--- ex/ex014/script.js
+++ ex/ex014/script.js
@@ -8,11 +8,12 @@ function addNum() {
     var opt = document.createElement('option')
     opt.text = `Valor ${numero} adicionado`
     selec.appendChild(opt)
-    span_result.innerHTML = numeros
 }
 
 function analisarVetor() {
+    var soma = 0
     for (let pos in numeros) {
+        soma += numeros[pos]
         if (numeros[pos] == numeros[0]) {
             var maior = numeros[pos]
             var menor = numeros[pos]
@@ -27,8 +28,11 @@ function analisarVetor() {
         }
     }
 
+    var media = soma/numeros.length
     span_result.innerHTML += `Ao todo temos ${numeros.length} números cadastrados <br>`
     span_result.innerHTML += `O maior valor foi ${maior} <br>`
     span_result.innerHTML += `O menor valor foi ${menor} <br>`
-    span_result.innerHTML += ``
+    span_result.innerHTML += `A soma dos valores foi ${soma} <br>`
+    span_result.innerHTML += `A média de todos os números foi ${media} <br>`
+
 }
