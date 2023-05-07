a2260b11c2ee317b637caf32f0a5706ffc9946b7
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-05-06 23:37:05 -0300

    Object Destructuring

---
+++ ES/ES07/script.js
@@ -0,0 +1,15 @@
+const address = {
+    street: '',
+    city: '',
+    country: ''
+}
+
+const { street, city, country} = address;
+// caso queira mudar o nome
+const {street: st} = address;
+
+// equivalente a
+
+const _street = address.street;
+const _city = address.city;
+const _country = address.country;
