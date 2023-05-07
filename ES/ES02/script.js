3a888289e1b4eaa3ae9d5c0efa9a69d92d59517a
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-05-06 21:36:27 -0300

    object

--- ES/ES02/script.js
+++ ES/ES02/script.js
@@ -3,3 +3,13 @@ const pessoa = {
     andar: function() {},
     conversar() {} // sintax mais simples do que a anterior
 }
+
+pessoa.talk;
+pessoa.nome;
+
+/**
+ * Outra maneira de acessar propriedades melhor
+ */
+
+const targetMember = 'name';
+pessoa[targetMember.value] = 'Lucas'; // caso targetMember seja um input, podemos acessar o valor dele dinamicamente assim.
