3c47d4dbbde10b3c395b9f5b82602be0d9e82c25
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-05-06 22:12:57 -0300

    bind()

--- ES/ES03/script.js
+++ ES/ES03/script.js
@@ -1,4 +1,4 @@
-//"use strict" - strict mode, basta colcar isso no começo do arquivo que ele vai ativar
+"use strict" //- strict mode, basta colcar isso no começo do arquivo que ele vai ativar
 
 const pessoa = {
     nome: "Joao",
@@ -7,7 +7,12 @@ const pessoa = {
 
 pessoa.andar();
 
-const andar = pessoa.andar;
+const andar = pessoa.andar.bind(pessoa); // retorna uma nova instancia da função andar e configura this para um objeto pessoa
 andar(); 
 // retorna um objeto window
 // se o strict mode está ativo, retorna undefined
+
+/**
+ * Dentro do JavaScript, funções são objetos, então andar() é um objeto
+ * bind() - o que passar de argumento para ele, irá determinar o valor de this
+ */
