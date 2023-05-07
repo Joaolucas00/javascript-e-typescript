74e0091567d8422d1dec25af8c898c6259567b9d
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-05-07 19:52:21 -0300

    Update script.js

--- ES6/ES10/script.js
+++ ES6/ES10/script.js
@@ -2,6 +2,8 @@ import { Teacher } from "./teacher";
 //ou
 import Teacher  from "./teacher";
 
+import Teacher, { promote } from "./teacher";
+
 const teacher = new Teacher("Lucas", "MSc");
 
 teacher.teach();
