d88c5d3df280019a10f613938699aecf41e8bc40
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-05-06 23:28:43 -0300

    Array.map

---
+++ ES/ES06/script.js
@@ -0,0 +1,8 @@
+const cores = ['azul', 'vermelho', 'verde'];
+const itens = cores.map( cor => `<li>${cor}</li>`);
+console.log(itens);
+
+/**
+ * .map() - Calls a defined callback function on each element of an array, and returns an array that contains the results.
+ * 
+ */
