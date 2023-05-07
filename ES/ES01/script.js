006c1440046d8b31e957e89ca1be8a5842e3f775
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-05-06 21:20:55 -0300

    var vs let

---
+++ ES/ES01/script.js
@@ -0,0 +1,24 @@
+function sayHello() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+
+    console.log(i); // a variável está definida aqui, dentro da função
+    /** let vs var
+     * A diferença está no escopo
+     * 
+     * var -> function
+     * let -> block
+     * 
+     * let está definida dentro do bloco onde a variável let é definida
+     * 
+     */
+
+    for (let j = 0; j < 5; j++) {
+        console.log(j); // j só funciona aqui
+    }
+
+    console.log(j); // erro, variável não está definida
+}
+
+sayHello();
