5026ebf62b4954712d96de5ae19737e49654d8d9
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-05-07 20:53:48 -0300

    generators

---
+++ ES6/generators/script.js
@@ -0,0 +1,40 @@
+function* generator() {
+    console.log("primeiro");
+    yield 1
+    console.log("secundo");
+    yield 2
+    console.log("Terceiro");
+    yield 3
+    console.log("Último");
+}
+
+const generatorObj = generator();
+console.log(generatorObj);
+
+console.log(generatorObj.next()); // pausa no yield 1 até o próximo yield
+console.log(generatorObj.next());
+console.log(generatorObj.next());
+console.log(generatorObj.next());
+
+/**
+ * next - execute the code inside of the generator
+ * 
+ */
+
+// Outro exemplo
+
+function* generateId() {
+    let id = 0;
+    while (true) {
+        yield id
+        id++
+    }
+}
+const obj = generateId();
+function ativaGenerator() {
+    /*
+    let idText = document.getElementById('id')
+    idText.innerHTML = obj.next().value
+    */
+    document.getElementById('id').innerHTML = obj.next().value
+}
