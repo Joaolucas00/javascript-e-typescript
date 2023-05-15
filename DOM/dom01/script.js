c0fd8b6af721a2d412345f7b51884d2cd103bf91
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-05-15 18:39:04 -0300

    .

---
+++ DOM/dom01/script.js
@@ -0,0 +1,9 @@
+//console.log(document.domain) pra mostrar o dominio
+//console.log(document.URL) mostra URL
+//console.log(document.title) mostra title
+//document.title = "items lister"
+//console.log(document.doctype);
+
+console.log(document.head)
+console.log(document.body)
+//console.log(document.all) depreciativo
