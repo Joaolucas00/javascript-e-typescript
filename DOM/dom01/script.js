c72c72d103d206f684c37008f148e1675baf959c
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-05-16 22:33:30 -0300

    querySelector e querySelectorAll

--- DOM/dom01/script.js
+++ DOM/dom01/script.js
@@ -16,4 +16,24 @@
 //console.log(document.getElementById('h1').innerText); // presta atenção do estilo
 //console.log(document.getElementById('h1').textContent); // mostra tudo que ta dentro
 
-console.log(document.getElementsByClassName("section"));
+//let section = document.getElementsByClassName("section");
+//console.log(section);
+//console.log(section[1])
+
+//console.log(document.querySelector('#header'));
+//console.log(document.querySelectorAll('.section'));
+
+//let lastSection = document.querySelector('.section:nth-child(1)');
+//console.log(lastSection);
+
+
+var odd = document.querySelectorAll('.section:nth-child(odd)');
+var even = document.querySelectorAll('.section:nth-child(even)');
+
+for (var i = 0; i < odd.length; i++) {
+    odd[i].style.backgroundColor = "green";
+}
+
+for (var i = 0; i < even.length; i++) {
+    even[i].style.backgroundColor = "yellow";
+}
