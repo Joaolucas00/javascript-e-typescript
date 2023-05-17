b5f39457a2b7793e388db642d970773603a153b3
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-05-16 21:30:08 -0300

    .

--- DOM/dom01/script.js
+++ DOM/dom01/script.js
@@ -4,6 +4,16 @@
 //document.title = "items lister"
 //console.log(document.doctype);
 
-console.log(document.head)
-console.log(document.body)
-//console.log(document.all) depreciativo
+//console.log(document.head)
+//console.log(document.body)
+//console.log(document.all) !! depreciativo !!
+
+//console.log(document.forms);
+//console.log(document.links); pega todos os links da página
+
+//console.log(document.images)
+
+//console.log(document.getElementById('h1').innerText); // presta atenção do estilo
+//console.log(document.getElementById('h1').textContent); // mostra tudo que ta dentro
+
+console.log(document.getElementsByClassName("section"));
