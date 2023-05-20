fb11345afef6c18e00049adf7047e719e95330b7
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-05-19 23:39:27 -0300

    Update script.js

--- DOM/dom02/script.js
+++ DOM/dom02/script.js
@@ -1,2 +1,21 @@
 var h2 = document.querySelector('#h2');
-console.log(h2.parentNode);
+var main = document.querySelector('main')
+
+// parent, child, first, last
+console.log(h2.parentElement.parentElement);
+console.log(main.childNodes); // text é um espaço vazio
+console.log(main.children); // então é melhor usar children
+console.log(main.children[1]);
+console.log(main.firstChild); // retorna text
+console.log(main.firstElementChild); // não retorna text
+
+console.log(main.lastChild);
+console.log(main.lastElementChild);
+
+// siblings
+
+
+console.log(main.nextSibling); // text
+console.log(main.nextElementSibling); // sem text
+console.log(main.previousSibling);
+console.log(main.previousElementSibling);
