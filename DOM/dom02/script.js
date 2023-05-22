ac4b176411e0f4f069bb334d12821f96cb7c5fbe
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2023-05-21 21:53:39 -0300

    Update script.js

--- DOM/dom02/script.js
+++ DOM/dom02/script.js
@@ -1,11 +1,13 @@
 var h2 = document.querySelector('#h2');
 var main = document.querySelector('main')
+var section = document.querySelector('section#form')
 
 // parent, child, first, last
 console.log(h2.parentElement.parentElement);
 console.log(main.childNodes); // text é um espaço vazio
 console.log(main.children); // então é melhor usar children
 console.log(main.children[1]);
+
 console.log(main.firstChild); // retorna text
 console.log(main.firstElementChild); // não retorna text
 
@@ -14,8 +16,23 @@ console.log(main.lastElementChild);
 
 // siblings
 
-
 console.log(main.nextSibling); // text
 console.log(main.nextElementSibling); // sem text
 console.log(main.previousSibling);
 console.log(main.previousElementSibling);
+
+var newDiv = document.createElement('div');
+
+newDiv.className = 'div';
+
+newDiv.id = 'id';
+
+newDiv.setAttribute('title', 'Ola'); // adiciona um atributo
+
+var divText = document.createTextNode('Olá Mundo');
+
+newDiv.appendChild(divText);
+
+console.log(newDiv);
+
+main.insertBefore(newDiv, section)
