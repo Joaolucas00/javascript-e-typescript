1df049a6b21b445c737690d3df8e3900d66a49aa
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2024-01-08 16:27:54 -0300

    camel case para snake case

--- ex/snake_case-toCamelCase/script.js
+++ ex/snake_case-toCamelCase/script.js
@@ -1,18 +1,20 @@
-function snakeCasetoCamelCase (nome) {
-    if (nome.includes("_") || nome.includes("-")) {
+function snakeCasetoCamelCase (str) {
+    if (str.includes("_") || str.includes("-")) {
         let array = []
-        for (let n = 0; n < nome.length; n++) {
-            array.push(nome[n])
+        for (let n = 0; n < str.length; n++) {
+            array.push(str[n])
         }
         for (let v = 0; v < array.length; v++) {
             if (array[v] == "_" || array[v] == "-") {
-                array.splice(array.indexOf(array[v]), 1)
+                array.splice(v, 1)
                 array[v] = array[v].toUpperCase()
             }
         }
-        return nome = array.join("")
+        return str = array.join("")
     }
     return 0;
 }
+
+
 
 console.log(snakeCasetoCamelCase("snake_case-to_Camel-case"))
---
+++ ex/snake_case-toCamelCase/script02.js
@@ -0,0 +1,18 @@
+function camelCaseTo_snake_case (str) {
+    if (/[A-Z]/.test(str)) {
+        let array = []
+        for (let n = 0; n < str.length; n++) {
+            array.push(str[n])
+        }
+        for (let v = 0; v < array.length; v++) {
+            if (/[A-Z]/.test(array[v])) {
+                array[v] = array[v].toLowerCase()
+                array.splice(v, 0, "_")
+            }
+        }
+        return str = array.join("")
+    }
+    return 0
+}
+
+console.log(camelCaseTo_snake_case("camelCaseConverting"));
