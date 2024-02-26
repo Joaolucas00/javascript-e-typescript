f0209c195ef728b148ec36043f608f8a5f9c34fa
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2024-02-26 17:08:17 -0300

    Método limpar formulário

--- typescript-curso-1/app/controllers/negociacao-controller.ts
+++ typescript-curso-1/app/controllers/negociacao-controller.ts
@@ -13,13 +13,24 @@ export class NegociacaoController {
 
     adicionar(): void {
         const negociacao = this.criarNegociacao()
+        console.log(negociacao);
+        this.limparForm()
+        
     }
 
     criarNegociacao(): Negociacao {
         const exp = /-/g;
         const date = new Date(this.inputData.value.replace(exp, ','))
-        const quantidade = parseInt(this.inputQuantidade.value)
-        const valor = parseFloat(this.inputValor.value)
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
         return new Negociacao(date, quantidade, valor);
     }
+
+    limparForm(): void {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
+    }
+
 }
