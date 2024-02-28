c69800e2431b0364a1349d84f1cd57e15e025dd5
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2024-02-28 16:51:19 -0300

    decorators

--- typescript-curso-1/app/src/controllers/negociacao-controller.ts
+++ typescript-curso-1/app/src/controllers/negociacao-controller.ts
@@ -1,3 +1,4 @@
+import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
 import { DiasDaSemana } from "../enums/dias-da-semana.js";
 import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from "../models/negociacoes.js";
@@ -19,7 +20,7 @@ export class NegociacaoController {
         this.negociacoesView.update(this.negociacoes);
     }
 
-
+    @logarTempoDeExecucao()
     public adicionar(): void {
         const negociacao = Negociacao.criaDe(
             this.inputData.value,
@@ -34,8 +35,7 @@ export class NegociacaoController {
         this.negociacoes.adiciona(negociacao);
         this.limparForm();
         this.atualizaView();
-
-
+        
     }
 
 
--- typescript-curso-1/app/src/views/view.ts
+++ typescript-curso-1/app/src/views/view.ts
@@ -1,3 +1,5 @@
+import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
+
 export abstract class View<T> {
 
     protected elemento: HTMLElement;
@@ -17,13 +19,14 @@ export abstract class View<T> {
 
     protected abstract template(model: T): string;
 
-
+    @logarTempoDeExecucao()
     public update(model: T): void {
         let template = this.template(model)
         if (this.escapar) {
             template = template.replace(/<script>[\s\S]*?<\/script>/, '');
         }
         this.elemento.innerHTML = template
+        
     }
 
 }
