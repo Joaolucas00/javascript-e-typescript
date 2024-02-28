0a14592da4fe413852de58d5f10e7288a077ba14
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2024-02-28 14:26:00 -0300

    finalizando

--- typescript-curso-1/dist/js/models/negociacao.js
+++ typescript-curso-1/dist/js/models/negociacao.js
@@ -19,28 +19,3 @@ export class Negociacao {
         return data;
     }
 }
-/**
- *
- *     private _data: Date;
-    private _quantidade: number;
-    private _valor: number;
-
-    constructor(data: Date, quantidade: number, valor: number) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
-    }
-
-        get data(): Date {
-        return this._data
-    }
-
-    get quantidade(): number {
-        return this._quantidade
-    }
-
-    get valor(): number {
-        return this._valor
-    }
-
- */ 
--- typescript-curso-1/dist/js/models/negociacoes.js
+++ typescript-curso-1/dist/js/models/negociacoes.js
@@ -2,7 +2,6 @@ export class Negociacoes {
     constructor() {
         this.negociacoes = [];
     }
-    // private negociacoes: Negociacao[] mesma declaração acima
     adiciona(negociacao) {
         this.negociacoes.push(negociacao);
     }
