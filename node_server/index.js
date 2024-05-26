2469149b02951d813f76cdfac97879ec6987da58
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2024-05-26 19:29:49 -0300

    _

--- node_server/index.js
+++ node_server/index.js
@@ -3,31 +3,49 @@ const fs = require('fs')
 const rs = fs.createReadStream('./texto.txt')
 
 
+const server = {
+    sendHtml: (html, status, res, head) => {
+        fs.readFile(html, (err, data) => {
+            res.writeHead(status, {'Content-Type': 'text/html', ...head})
+            res.write(data)
+            return res.end()
+        })
+    }
+}
+
+const routes = {
+    acceptUrls: [
+        {url: '/', html: 'index.html'},
+        {url: '/about', html: 'about.html'}
+    ]
+}
+
 
 http.createServer((req, res) => {
 
-    fs.appendFile('texto.txt', 'teste', (err) => {
-        if(err) throw err
-        console.log('Saved');
-    })
+    if (!routes.acceptUrls.some((route) => route.url === req.url)) {
+        server.sendHtml('404.html', 404, res)
+    }
 
-    fs.readFile('index.html', (err, data) => {
-        res.writeHead(200, {'Content-Type': 'text/html'})
-        res.write(data)
-        return res.end()
+    routes.acceptUrls.forEach((route) => {
+        if (route.url === req.url) {
+            server.sendHtml(route.html, 200, res)
+        }
     })
     
 }).listen(5555)
 console.log("Servidor pronto em: http://localhost:5555/");
 
 http.createServer((req, res) => {
+
     res.writeHead(200, {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': 'http://localhost:5555'
     })
-    if(req.url === '/api') {
-        res.end(JSON.stringify({nome: "João", id: 19}))
-    }
+
+    res.end(JSON.stringify({nome: "João", id: 19}))
+
+    
 }).listen(8080)
 console.log("Servidor JSON pronto em: http://localhost:8080/");
 
