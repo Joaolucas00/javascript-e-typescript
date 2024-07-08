b918055838970c4033838fdba872f9e4ad51cc64
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2024-07-08 20:35:20 -0300

    refatorando

--- node_server/index.js
+++ node_server/index.js
@@ -2,7 +2,73 @@ const http = require('http')
 const fs = require('fs')
 const rs = fs.createReadStream('./texto.txt')
 
+const server = {
+    sendHtml: async (html, status, res, head = {}) => {
+        fs.readFile(html, (err, data) => {
+            if(err) throw err
+            fs.writeFile(html, data, (err) => {
+                if (err) throw err
+                res.writeHead(status, {'Content-Type': 'text/html', ...head})
+                res.write(data)
+                return res.end()
+            })
+        })
+    }
+}
 
+
+const routes = {
+    '/': 'index.html',
+    '/about': 'about.html',
+    '/teste': 'teste.html'
+}
+
+
+http.createServer((req, res) => {
+    console.log("REQ: ", req.url);
+    console.log("routes[req.url]: ", routes[req.url]);
+    if(routes[req.url]) {
+        return server.sendHtml(routes[req.url], 200, res)
+    }
+
+    return server.sendHtml('404.html', 404, res)
+    
+}).listen(5555)
+console.log("Servidor pronto em: http://localhost:5555/");
+
+
+const controller = {
+    getJson: (req, res) => {
+        res.end(JSON.stringify({nome: "João", id: 19}))
+    }
+}
+
+const controllerBy = {
+    GET: controller.getJson
+}
+
+
+
+http.createServer((req, res) => {
+    
+    res.writeHead(200, {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'http://localhost:5555'
+    })
+    
+    if(controllerBy[req.method]) return controllerBy[req.method](req, res)
+    
+    
+}).listen(8080)
+console.log("Servidor JSON pronto em: http://localhost:8080/");
+
+rs.on('open', () => {
+    console.log('The file is open')
+})
+
+
+
+/*
 const server = {
     sendHtml: (html, status, res, head) => {
         fs.readFile(html, (err, data) => {
@@ -12,41 +78,17 @@ const server = {
         })
     }
 }
+*/
 
-const routes = {
-    acceptUrls: [
-        {url: '/', html: 'index.html'},
-        {url: '/about', html: 'about.html'}
-    ]
-}
-
-
-http.createServer((req, res) => {
-
-    if (!routes.acceptUrls.some((route) => route.url === req.url)) {
-        return server.sendHtml('404.html', 404, res)
-    }
-
-    routes.acceptUrls.forEach((route) => {
-        if (route.url === req.url) server.sendHtml(route.html, 200, res)
+/*
+function render(html, data, status, res, head) {
+    fs.write(html, data, () => {
+        if (err) throw err
+        res.writeHead(status, {'Content-Type': 'text/html', ...head})
+        res.write(serverRender.renderWithProps(data, () => {
+            return `<pre>${JSON.stringify(props)}</pre>`
+        }))
+        console.log("Opa!");
+        return res.end()
     })
-    
-}).listen(5555)
-console.log("Servidor pronto em: http://localhost:5555/");
-
-http.createServer((req, res) => {
-
-    res.writeHead(200, {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': 'http://localhost:5555'
-    })
-
-    res.end(JSON.stringify({nome: "João", id: 19}))
-
-    
-}).listen(8080)
-console.log("Servidor JSON pronto em: http://localhost:8080/");
-
-rs.on('open', () => {
-    console.log('The file is open')
-})
+}*/
